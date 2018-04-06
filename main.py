719881dd491c731e53b5ee07978484cb2c9ae674
NEO <hyangseok.chae@gmail.com>
2018-04-06 10:43:54 +0900

    Add Directory Path
    ex) python main.py path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pandas import Series, DataFrame
 import pandas as pd
 import glob, os
 import re
-import random
+import sys
 
 full_categories = ['Pss Total', 'Private Drity', 'Private Clean',
                    'SwapPss Dirty', 'Heap Size', 'Heap Alloc', 'Heap Free']
@@ -50,48 +50,65 @@ def fill_data_frame(dataframe, file, content):
 
     return None
 
+def convert_package_to_sheet(package):
+    target_chars = "[]:*?/\\"
+    for char in target_chars:
+        package = package.replace(char, "_")
+    return package[-28:]
+
 def save_to_excel(df, file):
     writer = pd.ExcelWriter(file, engine='xlsxwriter')
 
     packages = df['Package'].drop_duplicates().values.tolist()
     for package in packages:
+        if type(package) is not str:
+            continue
         package_sheet = df.ix[df['Package'] == package]
         package_sheet = package_sheet.sort_values(by=['Realtime'], ascending=[True])
         package_sheet.insert(0, 'Index', range(1, 1 + len(package_sheet)))
-        package_sheet.to_excel(writer, sheet_name=package+'_total')
+        sheet_name = convert_package_to_sheet(package)
+        package_sheet.to_excel(writer, sheet_name=sheet_name+'_A')
 
         #excel chart from : http://xlsxwriter.readthedocs.io/example_pandas_chart_line.html
         #chart only interest column
         package_sheet.set_index('Index', inplace=True)
         interest_column = [col for col in package_sheet.columns if 'Pss Total' in col]
         interest_sheet = package_sheet[interest_column]
-        interest_sheet.to_excel(writer, sheet_name=package)
+        interest_sheet.to_excel(writer, sheet_name=sheet_name)
 
         workbook = writer.book
-        worksheet = writer.sheets[package]
+        worksheet = writer.sheets[sheet_name]
         chart = workbook.add_chart({'type': 'line'})
 
         max_row = len(interest_sheet)
         for i in range(len(interest_sheet.columns)):
             col = i + 1
             chart.add_series({
-                'name': [package, 0, col],
-                'categories': [package, 1, 0, max_row, 0],
-                'values': [package, 1, col, max_row, col],
+                'name': [sheet_name, 0, col],
+                'categories': [sheet_name, 1, 0, max_row, 0],
+                'values': [sheet_name, 1, col, max_row, col],
             })
-
+        chart.set_title({
+            'name': package + " - Pss Total",
+            'overlay': True,
+            'layout': {
+                'x': 0.30,
+                'y': 0.02,
+            }
+        })
         chart.set_x_axis({'name': 'Runs'})
         chart.set_y_axis({'name': 'K Bytes', 'major_gridlines': {'visible': False}})
 
         # Insert the chart into the worksheet.
-        worksheet.insert_chart('G2', chart, {'x_offset':-500, 'y_offset':-500, 'x_scale': 3, 'y_scale': 2})
+        worksheet.insert_chart('G2', chart, {'x_offset':-500, 'y_offset':-500, 'x_scale': 2, 'y_scale': 1})
 
     writer.save()
 
-def main():
-    os.chdir("files")
+def main(dump_path):
+    os.chdir(dump_path)
     total_data = DataFrame()
     for file in glob.glob("*.txt"):
+        print("Processing ", file)
         f = open(file, 'r')
         lines = f.readlines()
         for line in lines:
@@ -102,5 +119,8 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+    dump_path = "files"
+    if len(sys.argv) > 1:
+        dump_path = sys.argv[1]
+    main(dump_path)
 
